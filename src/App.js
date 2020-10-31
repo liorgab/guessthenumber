801b99360ae247e1e9a3eed46fc7f0351580762d
lior gabay <liorgab@gmail.com>
2020-10-31 21:34:30 +0200

    input complete

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 function App() {
   const [myGuess,setMyGuess] = useState(0);
   const [rndNumber, setRndNumber] = useState(0);
-  const [history,setHistort] = useState('');
+  const [history,setHistory] = useState('');
   return (
     <div className="App">     
         <h1>1.2.3.</h1>
       <div>
         <input type="text"
-        placeholder="enter your guess ?"/>
+        placeholder="enter your guess ?"
+        onChange = {(e) => {setMyGuess(e.target.value)}}
+        value = {myGuess}/>
         <div>
           <button>check my guess</button>
           <button>restart</button>
