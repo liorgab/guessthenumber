8e6a0d3a92e464135e576ae047fd4af272a54825
lior gabay <liorgab@gmail.com>
2020-10-31 21:31:30 +0200

    input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [myGuess,setMyGuess] = useState(0);
+  const [rndNumber, setRndNumber] = useState(0);
+  const [history,setHistort] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App">     
         <h1>1.2.3.</h1>
-      </header>
+      <div>
+        <input type="text"
+        placeholder="enter your guess ?"/>
+        <div>
+          <button>check my guess</button>
+          <button>restart</button>
+        </div>
+
+
+      </div>
     </div>
   );
 }
